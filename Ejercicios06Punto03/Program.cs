73f04bb49154e2451648c63567901c1e5cdaca7e
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-06-30 18:38:17 -0300

    Ejercicio completo

--- Ejercicios06Punto03/Program.cs
+++ Ejercicios06Punto03/Program.cs
@@ -6,7 +6,30 @@ namespace Ejercicios06Punto03
     {
         static void Main(string[] args)
         {
-            Console.Title="Pasar nros base 10 a Binario";
+            Console.Title = "Pasar nros base 10 a Binario";
+            var contador = 0;
+            double nroBinario = 0;
+            var nroDecimal = 0;
+            var noSalir = true;
+            Console.Write("Ingrese un número en base 10:");
+            while (!int.TryParse(Console.ReadLine(), out nroDecimal))
+            {
+                Console.WriteLine("Trate otra vez con un nro entero");
+            }
+
+            do
+            {
+                var resto = nroDecimal % 2;
+                nroBinario =nroBinario +resto * Math.Pow(10,contador);
+                nroDecimal = nroDecimal / 2;
+                contador++;
+                if (nroDecimal < 2)
+                {
+                    nroBinario = nroBinario + nroDecimal * Math.Pow(10, contador);
+                    noSalir = false;
+                }
+            } while (noSalir);
+            Console.WriteLine($"Equivalente en binario es= {nroBinario}");
         }
     }
 }
