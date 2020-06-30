6a15078f069a3d2b7387c513aaa67d36c8a4f218
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-06-30 18:19:44 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicios06Punto03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicios06Punto03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="Pasar nros base 10 a Binario";
+        }
+    }
+}
